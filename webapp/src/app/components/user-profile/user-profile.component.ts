58a8ef17c76323586aeabc04049dbea1210a75d7
Ba Thi Truong <ba.truongthi@asnet.com.vn>
2022-06-28 08:19:30 +0700

    Implement ui upload and fix bug in PostConfirmation function

--- webapp/src/app/components/user-profile/user-profile.component.ts
+++ webapp/src/app/components/user-profile/user-profile.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,13 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  updateProfile($event: any): void {
+  async getS3SignedUrl(): Promise<any> {
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = {
+      method: 'GET',
+      headers,
+    };
 
+    const response =
+      await fetch(`https://3g6cuj6v71.execute-api.us-east-1.amazonaws.com/dev/assets/presignedUrlUpload`, options);
+
+    const presignedUrl = await response.json();
+
+    return presignedUrl;
   }
 
+  async pushPhotoToS3(presignedUploadUrl: string, file: File): Promise<any> {
+    const headers = new Headers({ 'Content-Type': 'image/*' });
+
+    return await fetch(presignedUploadUrl, {
+        method: 'PUT',
+        headers,
+        body: file
+    });
+  }
+
+  async handleUploadChange(e: any): Promise<void> {
+    const data = await this.getS3SignedUrl();
+
+    if (data.url) {
+      if (e.target.files) {
+        const file = e.target.files[0];
+        const result = await this.pushPhotoToS3(data.url, file);
+        console.log('LOG ME', result);
+      }
+    }
+  }
 }
